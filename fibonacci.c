02ba54b7784711f42990b65874bb22feed864884
abhishekchandra2522k <1ac23456789@gmail.com>
2019-10-26 00:43:30 +0530

    Added comments and modified the code

--- fibonacci.c
+++ fibonacci.c
@@ -1,8 +1,6 @@
-/* Printing the fibonacci series using recursive method*/
-
 #include<stdio.h>
 
-int fibonacci(int);    /* Declaration of Function fibonacci */
+int fibonacci(int); /* Declaration of function fibonacci*/
 
 int main(void)
 {
@@ -12,16 +10,19 @@ scanf("%d",&n);
 for(i=1;i<=n;i++)
 {
 printf("%d ",fibonacci(i));
-
 }
 return 0;
 }
 
 
-int fibonacci(int n)            /*Definition of function fibonacci*/
+int fibonacci(int n)
 {
-if(n>0)
+    if(n==1 || n==2)
+    {
+        return(1);
+    }
+if(n>2)
 {
-return(fib(n-1)+fib(n-2));
+return(fibonacci(n-1)+fibonacci(n-2));
 }
 }
